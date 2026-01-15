1493dcacb6175e46f745bd48e9ffed59172ba1e3
EvgeniyNakhay <qwert57894@gmail.com>
2026-01-16 01:23:19 +0300

    add: Favourites

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Authorization from "./pages/Authorization/Authorization";
 import MainPage from "./pages/Authorization/MainPage/MainPage";
+import Favourites from "./pages/Favourites/Favourites";
 import SearchResults from "./pages/SearchResults/SearchResults";
 
 function App() {
   return (
     <>
       {/* <Authorization /> */}
-      {/* <MainPage /> */}|
-      <SearchResults />
+      {/* <MainPage /> */}|{/* <SearchResults /> */}
+      <Favourites />
     </>
   );
 }
---
+++ src/pages/Favourites/Favourites.jsx
@@ -0,0 +1,114 @@
+import { Layout, List, Button } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+// import React, { useEffect, useState } from "react";
+import "./style.css";
+import MainHeader from "../../components/MainHeader/MainHeader";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setIsEditModalOpen } from "../../redux/actions/isEditModalOpen";
+// import EditModalWind from "../../components/ModalWind/EditModalWind";
+// import {
+//   addToFavourites,
+//   deleteFavRequest,
+// } from "../../redux/actions/favouritesAction";
+// import { activeFavRequest } from "../../redux/actions/activeFavRequest";
+// import { NavLink } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import { setSearchTerm } from "../../redux/actions/searchTermAction";
+const { Content } = Layout;
+
+const Favourites = () => {
+  //   const navigate = useNavigate();
+  //   const [requests, setRequests] = useState(null);
+  //   const favourites = useSelector((store) => store.favourites);
+  //   const isEditModalOpen = useSelector((store) => store.isEditModalOpen);
+  //   const dispatch = useDispatch();
+
+  //   const handleEdit = (id, searchTerm, favRequest, sortByF, maxResult) => {
+  //     dispatch(activeFavRequest(id, searchTerm, favRequest, sortByF, maxResult));
+  //     // dispatch(setIsEditModalOpen(true));
+  //   };
+
+  //   const searchFavRequest = (id, searchTerm, favRequest, sortByF, maxResult) => {
+  //     dispatch(activeFavRequest(id, searchTerm, favRequest, sortByF, maxResult));
+  //     dispatch(setSearchTerm(searchTerm));
+  //     navigate(`/search/${searchTerm}`);
+  //   };
+
+  return (
+    <>
+      <Layout style={{ height: "100vh" }}>
+        <MainHeader />
+        <>
+          <Content
+            style={{
+              height: "100vh",
+              paddingInline: "200px",
+            }}
+          >
+            <Content>
+              <h1
+                style={{
+                  fontSize: "36px",
+                  marginTop: "40px",
+                }}
+              >
+                Избранное
+              </h1>
+            </Content>
+            <List
+              style={{
+                background: "#FFF",
+              }}
+              size="large"
+              //   dataSource={favourites}
+              renderItem={(item) => (
+                <List.Item style={{ justifyContent: "space-between" }}>
+                  <Button
+                    type="text"
+                    style={{ fontWeight: 500, color: "#000000", padding: 0 }}
+                    // onClick={() =>
+                    //   searchFavRequest(
+                    //     item.id,
+                    //     item.searchTerm,
+                    //     item.favRequest,
+                    //     item.sortByF,
+                    //     item.maxResult
+                    //   )
+                    // }
+                  >
+                    {item.favRequest}
+                  </Button>
+                  <div>
+                    <Button
+                      //   onClick={() =>
+                      //     handleEdit(
+                      //       item.id,
+                      //       item.searchTerm,
+                      //       item.favRequest,
+                      //       item.sortByF,
+                      //       item.maxResult
+                      //     )
+                      //   }
+                      style={{ marginLeft: 20 }}
+                      type="primary"
+                      icon={<EditOutlined />}
+                    />
+                    <Button
+                      //   onClick={() => dispatch(deleteFavRequest(item.id))}
+                      style={{ marginLeft: 20 }}
+                      type="primary"
+                      icon={<DeleteOutlined />}
+                    />
+                  </div>
+                </List.Item>
+              )}
+            />
+          </Content>
+          {/* {isEditModalOpen && <EditModalWind />} */}
+        </>
+      </Layout>
+    </>
+  );
+};
+
+export default Favourites;

0de8f8f726752caf8444f59123f244d85b43b65f
EvgeniyNakhay <qwert57894@gmail.com>
2026-02-02 00:08:54 +0300

    fixed: styles for MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -5,24 +5,14 @@ import MainInput from "../../components/MainInput/MainInput";
 const { Header, Content, Footer } = Layout;
 
 const MainPage = () => {
-  //   const {
-  //     token: { colorBgContainer },
-  //   } = theme.useToken();
-
   return (
-    <Layout>
+    <Layout
+      style={{
+        height: "100vh",
+      }}
+    >
       <MainHeader />
-      <Content style={{ padding: "0 48px" }}>
-        <div
-          style={{
-            padding: 24,
-            minHeight: 380,
-            // background: colorBgContainer,
-          }}
-        >
-          <MainInput />
-        </div>
-      </Content>
+      <MainInput />
     </Layout>
   );
 };

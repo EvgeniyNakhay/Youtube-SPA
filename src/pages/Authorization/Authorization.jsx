2201c98b2f8b7c408a70f9909d26afe83999403d
EvgeniyNakhay <qwert57894@gmail.com>
2026-02-02 00:06:01 +0300

    add: styles css for Auth page

--- src/pages/Authorization/Authorization.jsx
+++ src/pages/Authorization/Authorization.jsx
@@ -1,7 +1,8 @@
 import logo from "./images/logo.svg";
+import "./style.css";
 
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Flex, Form, Input } from "antd";
+import { Button, Checkbox, Flex, Form, Input, Layout } from "antd";
 import { useNavigate } from "react-router";
 
 const Authorization = () => {
@@ -11,41 +12,43 @@ const Authorization = () => {
     navigate("/");
   };
   return (
-    <Form
-      name="login"
-      initialValues={{ remember: true }}
-      style={{ maxWidth: 360 }}
-      onFinish={onFinish}
-    >
-      <Form.Item name="logo">
-        <img src={logo} alt="logo" />
-      </Form.Item>
-      <Form.Item name="auth-caption">
-        <h1>Вход</h1>
-      </Form.Item>
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: "Введите логин!" }]}
+    <Layout className="layout">
+      <Form
+        name="login"
+        initialValues={{ remember: true }}
+        style={{ maxWidth: 360 }}
+        onFinish={onFinish}
       >
-        <Input prefix={<UserOutlined />} placeholder="Логин" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: "Введите пароль!" }]}
-      >
-        <Input.Password
-          prefix={<LockOutlined />}
-          type="password"
-          placeholder="Пароль"
-        />
-      </Form.Item>
+        <Form.Item name="logo">
+          <img src={logo} alt="logo" />
+        </Form.Item>
+        <Form.Item name="auth-caption">
+          <h1>Вход</h1>
+        </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Введите логин!" }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Логин" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Введите пароль!" }]}
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Пароль"
+          />
+        </Form.Item>
 
-      <Form.Item>
-        <Button block type="primary" htmlType="submit">
-          Войти
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </Layout>
   );
 };
 export default Authorization;
